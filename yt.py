80b3716c4bf00ea91c76c3b9405e816cf83a8168
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-24 13:28:45 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -4,5 +4,8 @@ import time
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(options=chrome_options)
-start_url = "https://www.youtube.com/watch?v=MR-MPwaN7Ns?autoplay=1"
+start_url = "https://www.youtube.com/watch?v=MR-MPwaN7Ns?&autoplay=1"
 driver.get(start_url)
+driver.get_screenshot_as_file("screenshot1.png")
+
+time.sleep(10000)
