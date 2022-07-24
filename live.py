e77ddbb5a4d9db5ba39a1717455b8ed554d12f87
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-24 10:54:19 +0530

    Update live.py

--- live.py
+++ live.py
@@ -3,6 +3,6 @@ from selenium.webdriver.chrome.options import Options
 import time
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
-ver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 start_url = "https://www.youtube.com/watch?v=MR-MPwaN7Ns?autoplay=1"
 driver.get(start_url)
