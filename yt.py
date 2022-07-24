43429dcf064b5d993f06530550626c4f5a3c225b
codem001 <108625927+codem001@users.noreply.github.com>
2022-07-24 11:54:53 +0530

    Update and rename live.py to yt.py

--- yt.py
+++ yt.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(options=chrome_options)
 start_url = "https://www.youtube.com/watch?v=MR-MPwaN7Ns?autoplay=1"
 driver.get(start_url)
